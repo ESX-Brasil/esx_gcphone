cce5c5fb9c59c8e3a59f0412c43ac45e437702b2
renildomarcio <krsolucoesweb@gmail.com>
2019-06-01 23:45:05 -0300

    up

--- gcphone/config.lua
+++ gcphone/config.lua
@@ -1,9 +1,9 @@
 FixePhone = {
-    -- Poste de police
-    ['911'] = { name =  "Central Police", coords = { x = 441.2, y = -979.7, z = 30.58 } },
+    -- Delegacia de polícia
+    ['911'] = { name =  "Polícia Central", coords = { x = 441.2, y = -979.7, z = 30.58 } },
 
-    -- Cabine proche du poste de police
-    ['008-0001'] = { name = "Cabine Telephonique", coords = { x = 372.25, y = -965.75, z = 28.58 } },
+    -- Cabine perto da delegacia de polícia
+    ['008-0001'] = { name = "Cabine Telefônica", coords = { x = 372.25, y = -965.75, z = 28.58 } },
 }
 
-ShowNumberNotification = true -- Show Number or Contact Name when you receive new SMS
+ShowNumberNotification = true -- Mostrar número ou nome do contato quando você receber novos SMS
